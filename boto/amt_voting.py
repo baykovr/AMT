1b1cdcf80e3810683b640428f16b0b017c1920a4
Robert Baykov <baykovr@gmail.com>
2014-03-24 00:13:15 -0500

    Cleaned up a bit/organized. Ready for links (for the most part)

--- boto/amt_voting.py
+++ boto/amt_voting.py
@@ -3,26 +3,27 @@
 from boto.mturk.connection import MTurkConnection
 from boto.mturk.question import QuestionContent,Question,QuestionForm,Overview,AnswerSpecification,SelectionAnswer,FormattedContent,Binary
 
-ACCESS_ID  = 'SECRET'
-SECRET_KEY = 'SECRET'
+ACCESS_ID  = 'secret'
+SECRET_KEY = 'secret'
 HOST       = 'mechanicalturk.sandbox.amazonaws.com'
 
 
-#"Does not yet implement Binary selection options" Yeah ok -Robert.
-def boto_injector(img_links):
-# A HIT is just xml, so boto is just an xml generator really.
 
+def boto_injector(img_links):
+# Extending boto to Binary datatype in SelectionAnswer
+# Generates radiobutton option per link
 	inject_thumb_size = 'l' # from http://api.imgur.com/models/image
 	inject_img_type   = 'png'
-	inject_pre  = '<MimeType><Type>image</Type><SubType>'+inject_img_type+'</SubType></MimeType><DataURL>'
+	inject_pre  = '<MimeType><Type>image</Type><SubType>'+\
+		inject_img_type+'</SubType></MimeType><DataURL>'
 	inject_post = '</DataURL><AltText>Letter Image</AltText>'
-	
 	radio_options = []
 	for index, item in enumerate(img_links):
-		#of form (link,0) 
-		link = inject_pre+item+inject_thumb_size+"."+inject_img_type+inject_post
+		link = inject_pre+item+inject_thumb_size+"."+\
+			inject_img_type+inject_post
 		radio_options.append( (link,index) )
-	return radio_options
+	# SelectionAnswer(...selections=radio_options...)
+	return radio_options 
 		
 
 def load_links(link_file):
@@ -77,42 +78,43 @@ def create_HIT(mturk_conn,letter,imgur_links):
 		print "[Exiting]"
 		exit(1)
 	
-	hit = None
-	return hit
-
-def main():
-	mturk_conn = connect_AMT()
-	img_dict = load_links("A.imgur")
-
-	# -- HIT Properties
-	title       = 'Vote on the best letter'
-	description = ('View the images and choose the letter which looks the best')
-	keywords    = 'image, voting, opinions'
-	#ratings =[('Very Bad','0'),('Bad','1'),('Not bad','2'),('Good','3'),('Very Good','4')]
+	hit = None	
+	#-HIT Properties
+	title      = 'Vote on the best letter'
+	description= ('View the images and choose the letter which looks the best')
+	keywords   = 'image, voting, opinions'	
 	
-	#ratings = [('<MimeType><Type>image</Type><SubType>png</SubType></MimeType><DataURL>http://i.imgur.com/y3kU3ffl.png</DataURL><AltText>The game board, with X to move.</AltText>','0')]
-	
-	ratings = boto_injector(img_dict['A'])
-	print ratings
-	#---------------  BUILD OVERVIEW ------------------- 
+	#-Question Overview
 	overview = Overview()
 	overview.append_field('Title', 'Choose the best looking letter')
-	#---------------  BUILD QUESTION 1 -------------------
- 
+	
+	#-Question
 	qc1 = QuestionContent()
 	qc1.append_field('Title','Select Letter')
-	#qc1.append(Binary(type='image',subtype='png',url='http://i.imgur.com/y3kU3ff.png',alt_text='X'))
 	
-	fta1 = SelectionAnswer(min=1, max=1,style='radiobutton',selections=ratings,type='binary',other=False)
-	print fta1.get_as_xml()
-	q1   = Question(identifier='design',content=qc1,answer_spec=AnswerSpecification(fta1),is_required=True)
- 
-	#--------------- BUILD THE QUESTION FORM -------------------
+	# Generate Awnsers 1 per imgur_links[]
+	choices = boto_injector(imgur_links)
+	
+	#-Awnser Choices
+	fta1 = SelectionAnswer(min=1, max=1,style='radiobutton',\
+		selections=choices,type='binary',other=False)
+	
+	q1 = Question(identifier='design',content=qc1,\
+		answer_spec=AnswerSpecification(fta1),is_required=True)
+	
+	#-Question Form
 	question_form = QuestionForm()
 	question_form.append(overview)
 	question_form.append(q1)
-	#--------------- CREATE THE HIT -------------------
-	mturk_conn.create_hit(questions=question_form,max_assignments=1,title=title,description=description,keywords=keywords,duration = 60*1,reward=0.00)
+	
+	#Put the HIT up
+	#mturk_conn.create_hit(questions=question_form,max_assignments=1,title=title,description=description,keywords=keywords,duration = 60*1,reward=0.00)
+
+def main():
+	mturk_conn = connect_AMT()
+	
+	img_dict = load_links("A.imgur")
+	create_HIT(mturk_conn,'A',img_dict['A'])
 
 if __name__ == "__main__":
     main()
